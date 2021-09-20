f84611a455d655b5e072af3a7ad8f5a6a3bd525c
Matt Koch <matthewjameskoch@gmail.com>
2021-09-20 11:29:44 -0400

    redirect urls based on code in route

--- app.py
+++ app.py
@@ -38,4 +38,11 @@ def your_url():
     else: 
         return redirect(url_for('home'))
 
-
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():
+                    return redirect(urls[code]['url'])
