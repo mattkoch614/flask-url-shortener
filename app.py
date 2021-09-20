46cbfd89dc085d36bbd35c9bbf2cd9b105cc3ddc
Matt Koch <matthewjameskoch@gmail.com>
2021-09-20 10:49:51 -0400

    alert users with message flashing

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+import os.path
 
 app = Flask(__name__)
+app.secret_key = 'DFC8713DF4E971D3FCFDF363121C5'
 
 @app.route('/')
 def home():
@@ -9,6 +12,19 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {}
+
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken. Please select another name.')
+            return redirect(url_for('home'))
+
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else: 
         return redirect(url_for('home'))
