ce03aaa3970003de1fd6b4b019ac979967bd6aa5
Matt Koch <matthewjameskoch@gmail.com>
2021-09-20 11:19:29 -0400

    allow uploads

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'DFC8713DF4E971D3FCFDF363121C5'
@@ -22,7 +23,15 @@ def your_url():
             flash('That short name has already been taken. Please select another name.')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url': request.form['url']}
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save('/Users/matthewkoch/Code/flask-url-shortener/uploads/' + full_name)
+            urls[request.form['code']] = {'file': full_name}
+
+        
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
